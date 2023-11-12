1d2e319f932dcc68e01e956984b10b5695a8d13b
Fab <fabianjarrett@gmail.com>
2023-11-12 09:56:50 +0000

    Click on whole row for details

--- src/app/player-table/player-table.component.ts
+++ src/app/player-table/player-table.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PlayerDataService } from '../player-data.service';
 import { Player } from '../player';
 import { PlayerRowComponent } from '../player-row/player-row.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-player-table',
@@ -14,9 +15,14 @@ import { PlayerRowComponent } from '../player-row/player-row.component';
 export class PlayerTableComponent {
   players: Player[] = [];
   playerData = inject(PlayerDataService);
+  router = inject(Router);
   
   constructor() {
     this.players = this.playerData.getAllPlayers();
     this.playerData.onPlayerUpdate.subscribe(() => this.players = this.playerData.getAllPlayers());
+  }
+
+  onClick(player: Player) {
+    this.router.navigate(['/player', player.MSTID]);
   }
 }
